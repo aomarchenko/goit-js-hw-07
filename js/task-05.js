2fe5892518e5467dcc31f13de06f6ec6b03829d9
Oleksandr Marchenko <80917411+aomarchenko@users.noreply.github.com>
2021-08-16 15:18:38 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,13 @@
-const input = document.querySelector("#name-input");
-const span = document.querySelector("#name-output");
+const input = document.querySelector('#name-input');
+const span = document.querySelector('#name-output');
 
 input.addEventListener('input', onKeyPress);
+// console.log(input.value);
 
-function onKeyPress (){
+function onKeyPress() {
+  if (input.value.length > 0) {
     span.textContent = input.value;
+  } else {
+    span.textContent = 'незнакомец';
+  }
 }
