d762a63aeb9afe3cf20511408f1bde1a88be6db4
Oleksandr Marchenko <80917411+aomarchenko@users.noreply.github.com>
2021-08-16 15:19:21 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,13 @@
-const input = document.querySelector("#validation-input");
-input.addEventListener("blur", letsCheckInputLength)
-function letsCheckInputLength (){
-    if(input.value.length < 6){
-        input.classList.add("invalid")
-    }
-    else {
-        input.classList.add("valid")
-    }
+const input = document.querySelector('#validation-input');
+input.addEventListener('blur', letsCheckInputLength);
+input.addEventListener('focus', letsRemoveClass);
+function letsCheckInputLength() {
+  if (input.value.length === Number(input.getAttribute('data-length'))) {
+    input.classList.add('valid');
+  } else if (input.value.length !== Number(input.getAttribute('data-length'))) {
+    input.classList.add('invalid');
+  }
+}
+function letsRemoveClass() {
+  input.classList.remove('valid', 'invalid');
 }
